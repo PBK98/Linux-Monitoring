deeff37a62225862454018949cc509b2195aca98
pbk98 <bumkyu8425@naver.com>
2026-05-12 22:11:29 +0900

    main/ docker 개인 편의성을 위한 셸스크립트

---
+++ LinuxServer/docker.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#docker stop ubuntu-server
+#docker rm ubuntu-server
+#docker rmi ubuntu-image
+#docker build -t ubuntu-image .
+#docker run -d --name ubuntu-server ubuntu-image
+#docker run -dit --cap-add=NET_ADMIN --cap-add=NET_RAW -p 20022:20022 -p 15034:15034 --name ubuntu-server ubuntu-image
+#docker exec -it ubuntu-server bash
