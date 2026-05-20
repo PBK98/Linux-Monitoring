c40880684c91879c2a387cac877ca212f43b518c
박범규 <bumkyu84259392@c4r8s5.codyssey.kr>
2026-05-20 20:36:07 +0900

    create_vm.sh 수정

--- UbuntuVM/scripts/create_vm.sh
+++ UbuntuVM/scripts/create_vm.sh
@@ -19,8 +19,6 @@ fi
 
 echo "[INFO] Starting shell..."
 
-orb -m "$VM_NAME" sudo -i
+orb -m "$VM_NAME" sudo bash -lc 'apt update && apt install -y git && git clone https://github.com/PBK98/Linux-Monitoring.git'
 
-orb -m "$VM_NAME" bash -lc 'apt update && apt install -y git && git clone https://github.com/PBK98/Linux-Monitoring.git'
-
-orb shell -m "$VM_NAME"
+orb shell -m "$VM_NAME" sudo -i
